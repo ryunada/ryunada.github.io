7b2adb7789aea750b3d674a201f52136d08e4d49
SeungHo Ryu <87309905+ryunada@users.noreply.github.com>
2022-09-30 09:41:44 +0900

    test

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-/*! Mr. Green Jekyll Theme - v1.1.0 (https://github.com/MrGreensWorkshop/MrGreen-JekyllTheme)
- *  Copyright (c) 2022 Mr. Green's Workshop https://www.MrGreensWorkshop.com
- *  Licensed under MIT
- */ (function () {
+(function () {
   "use strict";
   $(function () {
     $(document).click(function (e) {
