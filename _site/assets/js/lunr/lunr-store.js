278584750ba0bbf7649750d93a5afb2f3c9ead0e
ryunada <rst3047@gmail.com>
2023-02-11 23:04:12 +0900

    post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,19 @@
-var store = []
+var store = [{
+        "title": "MachineLearning",
+        "excerpt":"머신러닝의 개념. 머신러닝 규칙을 일일이 프로그래밍하지 않아도 자동으로 데이터에서 규칙을 학습하는 알고리즘을 연구하는 분야 대표적인 머신러닝 라이브러리 : 사이킷런(Scikit-learn) 지도학습(Supervised Learning) 분류(Classification) 회귀(Regression) 추천 시스템 시각 / 음성 감지/ 인지 텍스트 분석, NLP 비지도 학습(Un-supervised Learning) 클러스터링 차원 축소 강화학습 강화 학습(Reinforcement Learning) MachineLearning Skill 머신러닝 개념 데이터 전처리 Numpy...","categories": ["MachineLearning"],
+        "tags": [],
+        "url": "/machinelearning/MachineLearning/",
+        "teaser": null
+      },{
+        "title": "Numpy",
+        "excerpt":"Numpy -&gt; 파이썬의 대표적인 배열(array) 라이브러리 -&gt; 벡터 및 행렬 연산에 있어 매우 편리한 기능을 제공 1. Numpy 정의 import numpy as np 2. Array 정의 및 형태 확인 type 확인 : type(~) 배열 구조 확인 : ~.shape 배열 차원 확인하기 : ~.ndim # array정의 array1 = np.array([1,2,3]) print(f\"array1 type...","categories": ["MachineLearning"],
+        "tags": [],
+        "url": "/machinelearning/Numpy/",
+        "teaser": null
+      },{
+        "title": "Pandas",
+        "excerpt":"Pandas 데이터 처리와 분석을 위한 라이브러리 행과 열로 이루어진 데이터 객체를 만들어 다룰 수 있음 대용량의 데이터들을 처리하는데 매우 편리 Pandas 자료구조 시리즈(Series) : 1차원 데이터프레임(DataFrame) : 2차원 패널(Panel) : 3차원 1. Pandas정의 # conda install pandas import pandas as pd 2. Pandas 다루기 - read_csv(‘경로’) 파일 불러오기 # pd.read_csc('경로')...","categories": ["MachineLearning"],
+        "tags": [],
+        "url": "/machinelearning/Pandas/",
+        "teaser": null
+      }]
